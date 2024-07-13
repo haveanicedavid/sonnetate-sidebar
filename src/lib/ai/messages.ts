a3319e7f3617b6fb02c274c4e33817d7c5c437d5
David Daniel <davedan7@gmail.com>
2024-07-13 17:19:26 -0400

    chore: "Remove tags" option from prompt.

--- src/lib/ai/messages.ts
+++ src/lib/ai/messages.ts
@@ -1,52 +1,7 @@
 import Anthropic from '@anthropic-ai/sdk'
 
-import { createSystemPrompt } from './prompts'
+import { SYSTEM_PROMPT } from './prompts'
 import { StreamingMarkdownTransformer } from './response-transformer'
-
-export async function* streamSummarization({
-  url,
-  apiKey,
-  tags,
-  prompt,
-}: {
-  url: string
-  apiKey?: string
-  tags?: string
-  prompt?: string
-}) {
-  if (!apiKey) throw new Error('API key is required')
-
-  const anthropic = new Anthropic({
-    apiKey,
-  })
-
-  const content = prompt
-    ? `look at the webpage ${url} and use it for the following prompt. Make sure you use it for the first heading you respond with. {{prompt}}:\n\n${prompt}`
-    : `summarize the following webpage:: ${url}`
-
-  const stream = await anthropic.messages.create({
-    model: 'claude-3-sonnet-20240229',
-    max_tokens: 1000,
-    temperature: 0.5,
-    system: createSystemPrompt(tags),
-    messages: [
-      {
-        role: 'user',
-        content,
-      },
-    ],
-    stream: true,
-  })
-
-  for await (const chunk of stream) {
-    if (
-      chunk.type === 'content_block_delta' &&
-      chunk.delta.type === 'text_delta'
-    ) {
-      yield chunk.delta.text
-    }
-  }
-}
 
 /**
  * Takes a prompt and returns a stream of markdown parsed to assign hierarchy
@@ -55,12 +10,10 @@ export async function* streamSummarization({
 export async function* streamTransformedMarkdown({
   url,
   apiKey,
-  tags,
   prompt,
 }: {
   url: string
   apiKey?: string
-  tags?: string
   prompt?: string
 }) {
   if (!apiKey) throw new Error('API key is required')
@@ -78,7 +31,7 @@ export async function* streamTransformedMarkdown({
     model: 'claude-3-sonnet-20240229',
     max_tokens: 1000,
     temperature: 0.5,
-    system: createSystemPrompt(tags),
+    system: SYSTEM_PROMPT,
     messages: [
       {
         role: 'user',
--- src/lib/ai/prompts.ts
+++ src/lib/ai/prompts.ts
@@ -1,20 +1,12 @@
-export function createSystemPrompt(tags?: string) {
-  const tagString = tags
-    ? '\n- Try to use the following topics for headings, but only if they make sense to describe content: [' +
-      tags +
-      ']'
-    : ''
-  return `You are a helpful AI assistant that responds accurately and tersely while still being thorough.
-${markdownRules}
-${tagString}`
-}
-
-const markdownRules = `
+export const SYSTEM_PROMPT = `You are a helpful AI assistant that responds accurately and tersely while still being thorough.
 Your response should be in markdown syntax with the following rules:
 
-- The first line should be a \`# Heading\` that captures the highest level topic with as few words as possible. if a {{prompt}} is provided, make sure to use what stands out in that prompt to make the first heading
-- the first paragraph under the heading should be a terse summary of the entire response
 - response should be formatted into markdown headings which are the shortest possible descriptor of what follows.
+- It is very important that the first line should be a \`# Heading\` 
+- The first heading should summarize the main theme of question in a few short words. if a {{prompt}} is provided, make sure to use what stands out in that prompt to make the first heading
+- only use one top level \`# Heading \` per response
+- Try to make headings general. The text should be readable as though it's a normal document, but should categorize things similar to how tags would
+- the first paragraph under the heading should be a terse summary of the entire response
 - Don't use forward-slashes in headings
 - always use two newlines between markdown objects, ie respond with \`# heading\\n\\nparagraph\` and not \`# heading\\nparagraph\`
 `
