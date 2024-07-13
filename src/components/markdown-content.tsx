7ce8b879bb95c4d283766d6a04da39d853852b85
David Daniel <davedan7@gmail.com>
2024-07-13 00:06:50 -0400

    chore: update markdown rendering

--- src/components/markdown-content.tsx
+++ src/components/markdown-content.tsx
@@ -2,12 +2,22 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import remarkWikiLink from 'remark-wiki-link'
 
+import { cn } from '@/lib/utils'
 import '@/styles/markdown.css'
 
-export function MarkdownContent({ content }: { content: string }) {
+export function MarkdownContent({
+  content,
+  disableLinks,
+  className,
+}: {
+  content: string
+  disableLinks?: boolean
+  className?: string
+}) {
   return (
-    <div className="markdown-content">
+    <div className={cn(className, { 'disable-links': disableLinks })}>
       <ReactMarkdown
+        className="markdown-content"
         remarkPlugins={[
           remarkGfm,
           [
