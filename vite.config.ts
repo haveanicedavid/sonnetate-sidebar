06ed432dd89c60c5ab2e1140d2e750931e108657
David Daniel <davedan7@gmail.com>
2024-07-19 21:08:58 -0400

    fix: trying to fix issues with build not loading in chrome

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,23 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
-// import checker from 'vite-plugin-checker'
 import manifest from './manifest.json'
 
 export default defineConfig({
   plugins: [
     react(),
-    // checker({
-    //   // e.g. use TypeScript check
-    //   typescript: true,
-    // }),
     crx({ manifest: manifest as ManifestV3Export }),
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
     },
   },
 })
